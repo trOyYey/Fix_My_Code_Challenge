69852d4844cd8e125bfa25bbc730a3d4b72668fc
trOyYey <troyzuwara@gmail.com>
2024-04-16 19:38:15 +0000

    whats wrong with my suare

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
-class square():
-    
+"""Square mod """
+class Square():
+    """ Square class """
     width = 0
     height = 0
 
@@ -12,7 +12,7 @@ class square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
         return (self.width * 2) + (self.height * 2)
@@ -22,7 +22,7 @@ class square():
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
