3aa5e6f160d5fcf59725af43a314634ccf5846b1
trOyYey <troyzuwara@gmail.com>
2024-05-15 21:52:51 +0000

    fix my code ALX

---
+++ 0x01-challenge/user.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+User class
+"""
+
+
+class User():
+    """ User class """
+
+    def __init__(self):
+        """ instance initialization """
+        self.__email = None
+
+    @property
+    def email(self):
+        """ email getter attribute """
+        return self.__email
+
+    @email.setter
+    def email(self, value):
+        """ email setter attribute """
+        if type(value) is not str:
+            raise TypeError("email must be a string")
+        self.__email = value
+
+
+if __name__ == "__main__":
+
+    u = User()
+    u.email = "john@snow.com"
+    print(u.email)
